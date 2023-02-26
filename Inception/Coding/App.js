9771beb889f926e04bf707483d036dcbb6436082
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-26 20:57:51 +0530

    minor-edit

--- Inception/Coding/App.js
+++ Inception/Coding/App.js
@@ -2,4 +2,4 @@ const headingFirst = React.createElement("h1", {class:"heading-one"}, "Heading 1
 const headingSecond = React.createElement("h1", {class:"heading-two"}, "Heading 2");
 const container = React.createElement("div", {id:"container"}, [headingFirst, headingSecond])
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(container)v
+root.render(container)

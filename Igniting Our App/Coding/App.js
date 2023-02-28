5b4a4185c14c54c37e2c282461681b03d2cf4c7a
Riya Tiwari <riyatiwari727@gmail.com>
2023-02-28 16:02:23 +0530

    Adding files

---
+++ Igniting Our App/Coding/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const headingFirst = React.createElement("h1", {className:"heading-one"}, "Heading 1");
+const headingSecond = React.createElement("h1", {className:"heading-two"}, "Heading 2");
+const container = React.createElement("div", {id:"container"}, [headingFirst, headingSecond])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(container)
